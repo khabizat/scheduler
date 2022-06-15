241b33e3af4447d876019070cfea82219610c7f3
khabizat <habizat22@gmail.com>
2022-06-15 19:42:33 -0400

    Build InterviewerListItem component

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import classNames from "classnames";
+import "components/InterviewerListItem.scss";
+
+function InterviewerListItem(props){
+
+  const interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected": props.selected,
+  });
+
+
+  return(
+    <li className={interviewerClass} 
+        onClick= {props.setInterviewer}
+        selected={props.selected}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
+
+export default InterviewerListItem;
