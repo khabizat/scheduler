70cb425ae6d10b455f8b3e84fd459b088de420b6
khabizat <habizat22@gmail.com>
2022-06-14 22:07:48 -0400

    Add a function that formats props.spots

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -14,10 +14,22 @@ function DayListItem(props) {
     "day-list__item--full": !props.spots
   });
 
+  const formatSpots = function(spots) {
+    if (!spots) {
+      return 'no spots remaining';
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`;
+    }
+    if (spots === 2 || spots === 3 || spots === 4 || spots===5) {
+      return `${spots} spots remaining`;
+    }
+  }
+
   return(
     <li className={dayClass}>
       <h2 onClick={()=>props.setDay(day)} selected={props.selected} className="text--regular">{props.name}</h2> 
-      <h3 className="text--light">{props.spots} still remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
