3b674b1cb812763341582e4581c6fa529c2b1e98
Habizat Sharapudinova <habizat@MacBook-Air-Habizat.local>
2022-06-14 20:35:48 -0400

    Add conditional classnames

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,13 +1,21 @@
 import React from "react";
+import classNames from "classnames";
+import "components/DayListItem.scss";
 
 function DayListItem(props) {
   const name = "Monday";
   const spots = 6;
   const selected = false;
   const day = "Tuesday";
+  
+
+  const dayClass = classNames("day-list__item", {
+    "day-list__item--selected": props.selected,
+    "day-list__item--full": !props.spots
+  });
 
   return(
-    <li>
+    <li className={dayClass}>
       <h2 onClick={()=>props.setDay(day)} selected={props.selected} className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{props.spots} still remaining</h3>
     </li>
