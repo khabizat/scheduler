ae27b88f546744c4d81616fb4f4b4ba2b06776c4
khabizat <habizat22@gmail.com>
2022-06-15 23:10:36 -0400

    Initial commit. Add Appoinment component with shared styles

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss"; 
+// import "src/components/Appointment/styles.scss"
+
+
+function Appointment(props){
+  return (
+    <article className="appointment"></article>
+  );
+}
+
+export default Appointment;
