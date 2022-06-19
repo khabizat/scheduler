e1d8609ae785e7a58511c835b4222f2f79c4e264
khabizat <habizat22@gmail.com>
2022-06-19 19:00:43 -0400

    Minor update

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -6,7 +6,6 @@ function DayListItem(props) {
   
   const day = props.name;
   
-
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": !props.spots
@@ -25,8 +24,8 @@ function DayListItem(props) {
   }
 
   return(
-    <li className={dayClass}>
-      <h2 onClick={()=>props.setDay(day)} selected={props.selected} className="text--regular">{props.name}</h2> 
+    <li className={dayClass} onClick={()=>props.setDay(day)} selected={props.selected}>
+      <h2  className="text--regular">{props.name}</h2> 
       <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
